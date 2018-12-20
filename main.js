4b5669101504ebb6ad488cf8449f853241c7d024
Daniel Howard <danjhoward@gmail.com>
2018-12-19 23:14:22 -0800

    App component

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./src/App.js";
 
 require("@babel/polyfill");
 
 const rootElement = document.getElementById("root");
 
 render(
-  <div>
-    this is a React element
-  </div>,
+  <App />,
   rootElement
 );
 
@@ -20,15 +19,3 @@ function log(content) {
 function logObject(obj) {
   chrome.devtools.inspectedWindow.eval(`console.log(${obj})`)
 }
-
-chrome.devtools.network.onRequestFinished.addListener(
-  function(request) {
-    if (request.request.url.substr(-4, 4) === "/rpc" && request.request.postData) {
-      console.log("JFHGKFJHG");
-      const json = JSON.parse(request.request.postData.text);
-      const responseJson = request.getContent((c) => logObject(c));
-      log(json.method);
-      logObject(JSON.stringify(json.params));
-    }
-  }
-);
--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    requests: [],
+    activeRequest: null,
+  }
+
+  handleRequest = (request) => {
+    if (request.request.url.substr(-4, 4) === "/rpc" && request.request.postData) {
+      const json = JSON.parse(request.request.postData.text);
+      const responseJson = request.getContent((c) => {
+        const requestObj = {
+          method: json.method,
+          params: json.params,
+          response: JSON.parse(c),
+        };
+
+        this.setState({ requests: [...this.state.requests, requestObj]});
+      });
+    }
+  }
+
+  setActiveRequest = (req) => this.setState({ activeRequest: req })
+
+  componentDidMount() {
+    chrome.devtools.network.onRequestFinished.addListener(this.handleRequest);
+  }
+
   render() {
+    const { requests, activeRequest } = this.state;
+
     return (
       <div>
-        this is a React element
+        {requests.map(req => (
+          <div onClick={() => this.setActiveRequest(req)}>{req.method}</div>
+        ))}
+
+        {activeRequest &&
+          <div>{JSON.stringify(activeRequest.params)}</div>
+        }
+
+
+        {activeRequest &&
+          <div>{JSON.stringify(activeRequest.response)}</div>
+        }
       </div>
     );
   }
 }
+
+export default App;
