4f986628634e300cba631660d4d92376fcf9789e
Daniel Howard <danjhoward@gmail.com>
2018-12-20 00:22:06 -0800

    Reduce js

--- devtools.js
+++ devtools.js
@@ -1,8 +1,1 @@
-chrome.devtools.panels.create("RPC",
-    "icon.png",
-    "left-panel.html",
-    function(panel) {
-      // code invoked on panel creation
-      console.log("this is da bundle");
-    }
-);
+chrome.devtools.panels.create("RPC", "icon.png", "left-panel.html");
